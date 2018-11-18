c216c9b56eff5dc046acd4023975fa36c4b8ab1c
Anya <ivlieva.anya@mail.ru>
2018-11-18 16:30:07 +0300

    четвертый коммит

--- lesson 2/1.js
+++ lesson 2/1.js
@@ -13,14 +13,46 @@ let appData = {
 for (let i = 0; i < 2; i++) {
     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
         b = +prompt('Во сколько обойдется?', '');
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+    if ((typeof (a)) != null && (typeof (b)) != null && isNaN(b) && 
         a != '' && b != '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
     }else{
-
+        alert("Произошла ошибка Введите заново");
+        --i;
     }
 };
+// let i=0;
+// while (i<2) {
+//     i++;
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = +prompt('Во сколько обойдется?', '');
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+//         a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     }else{
+//         alert("Произошла ошибка Введите заново");
+//         --i;
+//     }
+// };
+
+// let i=0;
+// do {
+//     i++;
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = +prompt('Во сколько обойдется?', '');
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+//         a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     }else{
+//         alert("Произошла ошибка Введите заново");
+//         --i;
+//     }
+// }
+// while (i<2);
+
 
 appData.moneyPerDay = appData.budget / 30;
 
