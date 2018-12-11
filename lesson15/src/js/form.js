a1e967d39c07e0c77f864f6817af9a6058729a02
Anya <ivlieva.anya@mail.ru>
2018-12-11 07:58:43 +0300

    11 коммит

--- lesson15/src/js/form.js
+++ lesson15/src/js/form.js
@@ -10,23 +10,25 @@ export function Form() {
 
     let form = document.querySelector('.main-form'),
         input = form.getElementsByTagName('input'),
-        statusMessange = document.createElement('div');
-
+        statusMessange = document.createElement('div'),
+        inputLastForm = document.querySelectorAll('#form input'),
+        lastForm = document.querySelector('#form');
     statusMessange.classList.add('status');
 
-    function tel() {
-        input[0].addEventListener('input', function (event) {
+    function phoneNumberVerification(input) {
+        input.addEventListener('input', function (event) {
             let str = '0123456789+-)(';
-            if (str.indexOf(event.data, 0) === -1 || input[0].value.length > 16) {
-                input[0].value.length > 16 ? statusMessange.textContent = 'Длинна номера не более 16 симв.' : statusMessange.textContent = 'Можно вводить только цифры и символы +- ( )';
-                input[0].value = input[0].value.substr(0, input[0].value.length - 1);
+            if (str.indexOf(event.data, 0) === -1 || input.value.length > 16) {
+                input.value.length > 16 ? statusMessange.textContent = 'Длинна номера не более 16 симв.' : statusMessange.textContent = 'Можно вводить только цифры и символы +- ( )';
+                input.value = input.value.substr(0, input.value.length - 1);
                 form.appendChild(statusMessange);
             } else {
                 statusMessange.textContent = '';
             }
         });
     }
-    tel();
+    phoneNumberVerification(input[0]);
+    phoneNumberVerification(inputLastForm[1]);
 
     function sendForm(form) {
         form.addEventListener('submit', function (event) {
@@ -52,18 +54,20 @@ export function Form() {
             }
 
             function clearInput() {
+                let input = this.querySelectorAll('input');
                 for (let i = 0; i < input.length; i++) {
                     input[i].value = '';
                 }
             }
-
+            let clearForm = clearInput.bind(this);
 
             let formData = new FormData(form);
             postData(formData)
                 .then(() => statusMessange.innerHTML = messange.success)
                 .catch(() => statusMessange.innerHTML = messange.failur)
-                .then(() => clearInput());
+                .then(() => clearForm());
         });
     }
     sendForm(form);
+    sendForm(lastForm);
 }
